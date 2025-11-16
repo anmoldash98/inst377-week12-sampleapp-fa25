4b77879eeba8f6c90aa96d8d18a9097d1e000510
Anmol Dash <anmoldash98@gmail.com>
2025-11-16 00:50:22 -0500

    feat: Adding feature to add customers

--- public/Customers.js
+++ public/Customers.js
@@ -1,8 +1,25 @@
+async function createCustomer() {
+  await fetch(`/customer`, {
+    method: 'POST',
+    body: JSON.stringify({
+      firstName: `${document.getElementById('firstName').value}`,
+      lastName: `${document.getElementById('lastName').value}`,
+      state: `${document.getElementById('state').value}`,
+    }),
+    headers: {
+      'content-type': 'application/json',
+    },
+  }).then((result) => result.json());
+
+  await loadCustomerData();
+}
+
 async function loadCustomerData() {
   await fetch('/customers')
     .then((result) => result.json())
     .then((resultJson) => {
       const table = document.createElement('table');
+      table.setAttribute('id', 'customerInfo');
       const tableRow = document.createElement('tr');
 
       const tableHeadingFirstName = document.createElement('th');
@@ -37,6 +54,11 @@ async function loadCustomerData() {
 
         table.appendChild(customerTableRow);
       });
+
+      const preExistingTable = document.getElementById('customerInfo');
+      if (preExistingTable) {
+        preExistingTable.remove();
+      }
 
       document.body.appendChild(table);
     });
